6c668fe2a5c215a10d03722853a0338519045afa
Timothée Barray <timothee@autrementlemail.com>
2010-06-03 17:33:07 +0200

    mise en place module de demo pour premiers tests

---
+++ modules/zzFbSdkDemo/actions/actions.class.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of the .
+ * (c) 2010 Autrement
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * zzFbSdkDemoActions does things.
+ *
+ * @package    
+ * @subpackage actions
+ * @author     Autrement
+ * @version    1.0.0
+ */
+class zzFbSdkDemoActions extends sfActions
+{
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->setLayout(false);
+  }
+}
